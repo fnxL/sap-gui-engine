9438381a05e5ae642f11213aba4e76ad093119a1
fnxL <48607132+fnxL@users.noreply.github.com>
2025-10-21 18:45:19 +0530

    refactor: GuiComponent and GuiVComponent to use direct attributes instead of property-based access

--- src/sap_gui_engine/objects/gui_component.py
+++ src/sap_gui_engine/objects/gui_component.py
@@ -16,69 +16,9 @@ class GuiComponent:
         Args:
             element: The COM object representing the SAP GUI element
         """
-        self._container_type = element.ContainerType
-        self._id = element.id
-        self._name = element.name
-        self._parent = element.parent
-        self._type = element.type
-        self._type_as_number = element.TypeAsNumber
-
-    @property
-    def container_type(self) -> str:
-        """
-        Get the container type of the GUI component.
-
-        Returns:
-            The container type of the element
-        """
-        return self._container_type
-
-    @property
-    def id(self) -> str:
-        """
-        Get the ID of the GUI component.
-
-        Returns:
-            The ID of the element
-        """
-        return self._id
-
-    @property
-    def name(self) -> str:
-        """
-        Get the name of the GUI component.
-
-        Returns:
-            The name of the element
-        """
-        return self._name
-
-    @property
-    def parent(self) -> Any:
-        """
-        Get the parent COM object of the GUI component.
-
-        Returns:
-            The parent object of the element
-        """
-        return self._parent
-
-    @property
-    def type(self) -> str:
-        """
-        Get the type of the GUI component.
-
-        Returns:
-            The type of the element
-        """
-        return self._type
-
-    @property
-    def type_as_number(self) -> int:
-        """
-        Get the type of the GUI component as a number.
-
-        Returns:
-            The type of the element as a number
-        """
-        return self._type_as_number
+        self.container_type = element.ContainerType
+        self.id = element.id
+        self.name = element.name
+        self.parent = element.parent # The parent COM object
+        self.type = element.type
+        self.type_as_number = element.TypeAsNumber
--- src/sap_gui_engine/objects/gui_vcomponent.py
+++ src/sap_gui_engine/objects/gui_vcomponent.py
@@ -1,6 +1,5 @@
 import logging
 from typing import Any
-from sap_gui_engine.exceptions import ComboBoxOptionNotFoundError
 from .gui_component import GuiComponent
 
 logger = logging.getLogger(__name__)
@@ -26,24 +25,24 @@ class GuiVComponent(GuiComponent):
 
     def __init__(self, element: Any):
         super().__init__(element)
-        self._element = element
-        self._acc_label_collection = element.AccLabelCollection
-        self._acctext = element.AccText
-        self._acctext_on_request = element.AccTextOnRequest
-        self._acc_tooltip = element.AccTooltip
-        self._changeable = element.Changeable
-        self._default_tooltip = element.DefaultTooltip
-        self._height = element.Height
-        self._icon_name = element.IconName
-        self._is_symbol_font = element.IsSymbolFont
-        self._left = element.Left
-        self._modified = element.Modified
-        self._parent_frame = element.ParentFrame
-        self._screen_left = element.ScreenLeft
-        self._screen_top = element.ScreenTop
-        self._tooltip = element.Tooltip
-        self._top = element.Top
-        self._width = element.Width
+        self._element = element # The COM object itself
+        self.acc_label_collection = element.AccLabelCollection
+        self.acc_text = element.AccText
+        self.acc_text_on_request = element.AccTextOnRequest
+        self.acc_tooltip = element.AccTooltip
+        self.changeable = element.Changeable
+        self.default_tooltip = element.DefaultTooltip
+        self.height = element.Height
+        self.icon_name = element.IconName
+        self.is_symbol_font = element.IsSymbolFont
+        self.left = element.Left
+        self.modified = element.Modified
+        self.parent_frame = element.ParentFrame
+        self.screen_left = element.ScreenLeft
+        self.screen_top = element.ScreenTop
+        self.tooltip = element.Tooltip
+        self.top = element.Top
+        self.width = element.Width
 
     # Methods from SAP API
     def dump_state(self, inner_object: str = "") -> Any:
@@ -99,193 +98,21 @@ class GuiVComponent(GuiComponent):
         else:
             return self._element.Visualize(on)
 
-    @property
-    def acc_label_collection(self) -> Any:
-        """
-        Returns the collection of GuiLabel objects assigned to this control in the ABAP Screen Painter.
-
-        Returns:
-            GuiComponentCollection: Collection containing objects of type GuiLabel that were
-                                   assigned to this control in the ABAP Screen Painter.
-        """
-        return self._acc_label_collection
-
-    @property
-    def acc_text(self) -> str:
-        """
-        Returns an additional text for accessibility support.
-
-        Returns:
-            str: Additional text for accessibility support
-        """
-        return self._acctext
-
-    @property
-    def acc_text_on_request(self) -> str:
-        """
-        Returns an additional text for accessibility support that is shown on request.
-
-        Returns:
-            str: Additional text for accessibility support shown on request
-        """
-        return self._acctext_on_request
-
-    @property
-    def acc_tooltip(self) -> str:
-        """
-        Returns an additional tooltip text for accessibility support.
-
-        Returns:
-            str: Additional tooltip text for accessibility support
-        """
-        return self._acc_tooltip
-
-    @property
-    def changeable(self) -> bool:
-        """
-        Returns whether the object is changeable (not disabled or read-only).
-
-        Returns:
-            bool: True if the object is changeable, False otherwise
-
-        Note: An object is changeable if it is neither disabled nor read-only.
-        """
-        return self._changeable
-
-    @property
-    def default_tooltip(self) -> str:
-        """
-        Returns the tooltip text generated from the short text defined in the data dictionary
-        for the given screen element type.
-
-        Returns:
-            str: Tooltip text generated from the data dictionary
-        """
-        return self._default_tooltip
-
-    @property
-    def height(self) -> int:
-        """
-        Returns the height of the component in pixels.
-
-        Returns:
-            int: Height of the component in pixels
-        """
-        return self._height
-
-    @property
-    def icon_name(self) -> str:
-        """
-        Returns the name of the icon assigned to the object, or empty string if none assigned.
-
-        Returns:
-            str: Name of the icon assigned to the object, or empty string if none
-        """
-        return self._icon_name
-
-    @property
-    def is_symbol_font(self) -> int:
-        """
-        Returns whether the component's text is visualized in the SAP symbol font.
-
-        Returns:
-            int: 1 if the component's text is in SAP symbol font, 0 otherwise
-
-        Note: The property is TRUE if the component's text is visualized in the SAP symbol font.
-        """
-        return self._is_symbol_font
-
-    @property
-    def left(self) -> int:
-        """
-        Returns the left position of the element in screen coordinates.
-
-        Returns:
-            int: Left position of the element in screen coordinates
-        """
-        return self._left
-
-    @property
-    def modified(self) -> bool:
-        """
-        Returns whether the object is modified (state changed by user but not sent to SAP system).
-
-        Returns:
-            bool: True if the object is modified, False otherwise
-
-        Note: An object is modified if its state has been changed by the user and this change
-        has not yet been sent to the SAP system.
-        """
-        return self._modified
-
-    @property
-    def parent_frame(self) -> Any:
-        """
-        Returns the parent frame if the control is hosted by a Frame object, otherwise None.
-
-        Returns:
-            GuiComponent: The parent frame if the control is hosted by a Frame object, otherwise None
-
-        Note: If the control is hosted by the Frame object, the value of the property is this frame.
-        Otherwise NULL.
-        """
-        return self._parent_frame
-
-    @property
-    def screen_left(self) -> int:
-        """
-        Returns the y position of the component in screen coordinates.
-
-        Returns:
-            int: The y position of the component in screen coordinates
-        """
-        return self._screen_left
-
-    @property
-    def screen_top(self) -> int:
-        """
-        Returns the x position of the component in screen coordinates.
-
-        Returns:
-            int: The x position of the component in screen coordinates
-        """
-        return self._screen_top
-
-
-
-
-
-
-    @property
-    def tooltip(self) -> str:
-        """
-        Returns the tooltip text designed to help a user understand the meaning of a given
-        text field or button.
-
-        Returns:
-            str: The tooltip text for the component
-
-        Note: The tooltip contains a text which is designed to help a user understand the
-        meaning of a given text field or button.
-        """
-        return self._tooltip
-
-    @property
-    def top(self) -> int:
-        """
-        Returns the top coordinate of the element in screen coordinates.
-
-        Returns:
-            int: Top coordinate of the element in screen coordinates
-        """
-        return self._top
-
-    @property
-    def width(self) -> int:
-        """
-        Returns the width of the component in pixels.
-
-        Returns:
-            int: Width of the component in pixels
-        """
-        return self._width
+    # Properties as direct attributes (previously using @property)
+    # acc_label_collection: Returns the collection of GuiLabel objects assigned to this control in the ABAP Screen Painter.
+    # acc_text: Returns an additional text for accessibility support.
+    # acc_text_on_request: Returns an additional text for accessibility support that is shown on request.
+    # acc_tooltip: Returns an additional tooltip text for accessibility support.
+    # changeable: Returns whether the object is changeable (not disabled or read-only).
+    # default_tooltip: Returns the tooltip text generated from the short text defined in the data dictionary for the given screen element type.
+    # height: Returns the height of the component in pixels.
+    # icon_name: Returns the name of the icon assigned to the object, or empty string if none assigned.
+    # is_symbol_font: Returns whether the component's text is visualized in the SAP symbol font.
+    # left: Returns the left position of the element in screen coordinates.
+    # modified: Returns whether the object is modified (state changed by user but not sent to SAP system).
+    # parent_frame: Returns the parent frame if the control is hosted by a Frame object, otherwise None.
+    # screen_left: Returns the y position of the component in screen coordinates.
+    # screen_top: Returns the x position of the component in screen coordinates.
+    # tooltip: Returns the tooltip text designed to help a user understand the meaning of a given text field or button.
+    # top: Returns the top coordinate of the element in screen coordinates.
+    # width: Returns the width of the component in pixels.
