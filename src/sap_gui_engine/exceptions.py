48e24b79e20bf54a3d495a5205d58b3253601268
fnxL <48607132+fnxL@users.noreply.github.com>
2025-10-30 18:39:44 +0530

    feat: add login method to main engine class
    - add login method that performs SAP login with provided credentials only if it finds login elements, with all possible exceptions/scenarios handled
    - add login element mappings and create default login elements
    - fix property indentation in main engine class
    - create LoginError exception

--- src/sap_gui_engine/exceptions.py
+++ src/sap_gui_engine/exceptions.py
@@ -2,3 +2,9 @@ class TransactionError(Exception):
     """Raised when transaction code does not exist or Function is not possible."""
 
     pass
+
+
+class LoginError(Exception):
+    """Raised when login fails."""
+
+    pass
--- src/sap_gui_engine/mappings/__init__.py
+++ src/sap_gui_engine/mappings/__init__.py
@@ -1,3 +1,4 @@
 from .vkey import VKey
+from .login import LoginScreenElements, DEFAULT_LOGIN_ELEMENTS
 
-__all__ = ["VKey"]
+__all__ = ["VKey", "LoginScreenElements", "DEFAULT_LOGIN_ELEMENTS"]
